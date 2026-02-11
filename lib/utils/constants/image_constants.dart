58648f07a8c6931cec9fb442f9615a9a1aa1483d
taravannheng <hengtaravann@gmail.com>
2026-02-11 20:40:48 +0700

    chore: change app icon

---
+++ lib/utils/constants/image_constants.dart
@@ -0,0 +1,9 @@
+class ImageConstants {
+  static final String _basePath = 'assets/images';
+
+  // L
+  static final String logo = '$_basePath/logo.png';
+  static final String logoTransparent = '$_basePath/logo_transparent.png';
+  static final String logoGrayscale = '$_basePath/logo_grayscale.png';
+  static final String logoGrayScaleTransparent = '$_basePath/logo_grayscale_transparent.png';
+}
