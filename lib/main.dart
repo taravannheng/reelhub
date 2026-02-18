71eca156528d20eaa7621c0cffeddabc46f570cb
taravannheng <hengtaravann@gmail.com>
2026-02-19 02:44:05 +0700

    chore: add talker bloc observer for observing state transitions

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:talker_bloc_logger/talker_bloc_logger.dart';
 
 import 'package:reelhub/app.dart';
 import 'package:reelhub/config/di.dart';
@@ -11,6 +13,9 @@ void main() async {
   await dotenv.load(fileName: ".env");
   await initDI();
   final directory = await getApplicationDocumentsDirectory();
+  Bloc.observer = TalkerBlocObserver(
+    settings: const TalkerBlocLoggerSettings(enabled: kDebugMode),
+  );
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: HydratedStorageDirectory(directory.path),
   );
